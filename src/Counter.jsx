9496440cd1258880323dedc842a178e2743d2212
yosr <yassourayaakoubi@gmail.com>
2024-11-30 21:55:49 +0100

    first commit

---
+++ src/Counter.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Counter({ count, setCount }) {
+  return (
+    <div>
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <span>{count}</span>
+      <button onClick={() => setCount(count + 1)}>+</button>
+    </div>
+  );
+}
+
+export default Counter;
---
+++ src/Display.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Display({ value }) {
+  return <h2>Current Count: {value}</h2>;
+}
+
+export default Display;
